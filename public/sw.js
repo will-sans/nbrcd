68b068a943ca7b7fd7a5cda4d81141d0b594bfae
WILL <ryoheiyoneda@gmail.com>
2025-05-08 19:05:06 +0900

    sw modified

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
 // public/sw.js
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
-    self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-  });
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
