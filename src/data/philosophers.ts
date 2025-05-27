16c7cd74d9930d93774fc27c987ba7639b8db7de
WILL <ryoheiyoneda@gmail.com>
2025-05-27 22:22:27 +0900

    ogura added to philosopher

--- src/data/philosophers.ts
+++ src/data/philosophers.ts
@@ -1,5 +1,6 @@
 export const philosophers = [
-  { id: "drucker", name: "ドラッカー" },
-  { id: "matsushita", name: "松下幸之助" },
-  { id: "inamori", name: "稲盛和夫" },
+  { id: 'drucker', name: 'ドラッカー' },
+  { id: 'matsushita', name: '松下幸之助' },
+  { id: 'inamori', name: '稲盛和夫' },
+  { id: 'ogura', name: '小倉昌男' },
 ];
