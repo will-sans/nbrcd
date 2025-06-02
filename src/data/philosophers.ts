d86d2205f7a3f3543894f61dfb7ca6790a6b5d9e
WILL <ryoheiyoneda@gmail.com>
2025-06-02 14:01:24 +0900

    ezoe added

--- src/data/philosophers.ts
+++ src/data/philosophers.ts
@@ -4,4 +4,6 @@ export const philosophers = [
   { id: 'inamori', name: '稲盛和夫' },
   { id: 'ogura', name: '小倉昌男' },
   { id: 'collins', name: 'ジム・コリンズ' },
+  { id: 'dyson', name: 'ジェームス・ダイソン' },
+  { id: 'ezoe', name: '江副浩正' },
 ];
